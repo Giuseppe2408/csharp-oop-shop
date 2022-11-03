82f2daeb8774537215c4611d6da4c7afee524195
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-11-03 11:47:21 +0100

    aggiornato con get e set

--- Program.cs
+++ Program.cs
@@ -12,21 +12,28 @@ Console.WriteLine("funzionalità di reading");
 Console.WriteLine();
 
 //funzionalità di lettura
-Console.WriteLine("codice: " + newProduct.GetCode());
-Console.WriteLine("nome: " + newProduct.GetName());
-Console.WriteLine("descrzione: " + newProduct.GetDescription());
-Console.WriteLine("prezzo: " + newProduct.GetPrice());
-Console.WriteLine("iva: " + newProduct.GetIva());
+Console.WriteLine("codice: " + newProduct.Code);
+Console.WriteLine("nome: " + newProduct.Name);
+Console.WriteLine("descrzione: " + newProduct.Description);
+Console.WriteLine("prezzo: " + newProduct.Price);
+Console.WriteLine("iva: " + newProduct.Iva);
 
 Console.WriteLine();
 Console.WriteLine("funzionalità di setting");
 Console.WriteLine();
 
-//funzionalità di setting
-Console.WriteLine("nuovo nome: " + newProduct.SetName("libro nuovo"));
-Console.WriteLine("nuova descrizione: " + newProduct.SetDescription("libro ancora più bello"));
-Console.WriteLine("nuovo prezzo: " + newProduct.SetPrice(20));
-Console.WriteLine("nuova iva: " + newProduct.SetIva(10));
+//variabili di setting
+
+newProduct.Name = "nuovo lobrp";
+newProduct.Description = "nuovas descrizione";
+newProduct.Price = 22;
+newProduct.Price = 10;
+
+//stampa di setting
+Console.WriteLine("nuovo nome: " + newProduct.Name);
+Console.WriteLine("nuova descrizione: " + newProduct.Description);
+Console.WriteLine("nuovo prezzo: " + newProduct.Price);
+Console.WriteLine("nuova iva: " + newProduct.Iva);
 
 Console.WriteLine("il prezzo con l'iva è: " + newProduct.IvaPrice());
 
@@ -34,7 +41,7 @@ newProduct.ExtendendName();
 Console.WriteLine();
 //bonus
 Console.Write("Il codice esteso a 8 cifre è: ");
-Console.WriteLine(newProduct.ExtendCode());
+//Console.WriteLine(newProduct.ExtendCode());
 
 
 
@@ -54,87 +61,52 @@ Console.WriteLine(newProduct.ExtendCode());
 //iva
 public class Product
 {
-    private int code;
-    string name;
-    string description;
-    double price;
-    int iva;
+
+    
+
+   
+
+    public double price;
+    public int iva;
+
+
+    public int Code { get; }
+    public string Name { get; set;  }
+    public string Description { get; set;}
+    public double Price { get; set; }
+    public int Iva { get; set; }
 
     public Product ()
     {
         //Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter ed eventuali altri metodi di “utilità” per fare in modo che:
         //alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
-        this.code = new Random().Next(1, 1001);
+        this.Code = new Random().Next(1, 1001);
         
     }
 
     public Product(string name, string description,double price,int iva)
     {
         //this.code = 1000000;
-        this.code = new Random().Next(1, 10000001);
-        this.name = name;
-        this.description = description;
-        this.price = price;   
-        this.iva = iva;
+        this.Code = new Random().Next(1, 100);
+        this.Name = name;
+        this.Description = description;
+        this.Price = price;   
+        this.Iva = iva;
 
     }
 
     // disponibile solo in lettura 
-    public int GetCode()
-    {
-        return this.code;
-    }
+   
 
     //Gli altri attributi siano accessibili sia in lettura che in scrittura
-    public string GetName()
-    {
-        return this.name;
-    }
 
-    public string GetDescription()
-    {
-        return this.description;
-    }
 
-    public double GetPrice()
-    {
-        return this.price;
-    }
 
-    public double GetIva()
-    {
-        return this.iva;
-    }
-
-    public int SetCode(int code)
-    {
-        return this.code = code;
-    }
-
-    public string SetName(string name)
-    {
-        return this.name = name;
-    }
-
-    public string SetDescription(string description)
-    {
-        return this.description = description;
-    }
-
-    public double SetPrice(double price)
-    {
-        return this.price = price;
-    }
-
-    public double SetIva(int iva)
-    {
-        return  this.iva = iva;
-    }
     //Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
     public int IvaPrice()
     {
-        int conversion = Convert.ToInt32(this.price);
-        int perc = conversion * this.iva;
+        int conversion = Convert.ToInt32(this.Price);
+        int perc = conversion * this.Iva;
         int div = perc / 100;
         int result = conversion - div;
         return result;
@@ -143,23 +115,26 @@ public class Product
     public void ExtendendName()
     {
         //concatenare codice al nome
-        string conversion = Convert.ToString(this.code);
-        Console.WriteLine("code: " + this.code + " name: " + this.name);
+        string conversion = Convert.ToString(this.Code);
+        Console.WriteLine("code: " + this.Code + " name: " + this.Name);
     }
 
 
     //BONUS: create un metodo che restituisca il codice con un pad left di 0 per arrivare a 8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
-    public string ExtendCode()
-    {
-        string conversion = Convert.ToString(this.code);
-        int lunghezza = conversion.Length;
-        if (lunghezza < 8)
-        {
-            for (; lunghezza < 8; lunghezza++)
-            {
-                Console.Write("0");
-            }
-        }
-        return conversion;
-    }
+    //public string ExtendCode()
+    //{
+    //    string conversion = Convert.ToString(this.Code);
+    //    int lunghezza = conversion.Length;
+    //    string zero = "0";
+    //    string result = "";
+    //    if (lunghezza < 8)
+    //    {
+    //        for (i = 0; i < 8; i++)
+    //        {
+    //            result = zero + conversion;
+    //            //Console.Write("0");
+    //        }
+    //    }
+    //    return result;
+    //}
 }
